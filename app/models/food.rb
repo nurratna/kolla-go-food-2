c844e4a7a0622336d9c5cfb47ba2e15534a7f6cc
nurratna <nurratnasarii@gmail.com>
2017-10-26 16:46:47 +0700

    modifying models specs for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -7,7 +7,19 @@ class Food < ApplicationRecord
 		message: 'must be a URL for GIF, JPG or PNG image'
 	}
 
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	def self.by_letter(letter)
 		where("name LIKE ?", "#{letter}%").order(:name)
 	end
+
+	has_many :line_items
+
+	private
+		def ensure_not_referenced_by_any_line_item
+			unless line_items.empty?
+				errors.add(:base, 'Line Items present')
+				throw :abort
+			end
+		end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -4,4 +4,27 @@ describe LineItem do
 	it 'has a valid factory' do
 		expect(build(:line_item)).to be_valid
 	end
+
+	it 'deletes line_items when deleted' do
+		cart = create(:cart)
+		food1 = create(:food)
+		food2 = create(:food)
+
+		line_item1 = create(:line_item, cart: cart, food: food1)
+		line_item2 = create(:line_item, cart: cart, food: food2)
+		cart.line_items << line_item1
+		cart.line_items << line_item2
+
+		expect { cart.destroy }.to change { LineItem.count }.by(-2)
+	end
+
+	it "can't be destroyed while it has line_item(s)" do
+		cart = create(:cart)
+		food = create(:food)
+
+		line_item = create(:line_item, cart: cart, food: food)
+		food.line_items << line_item
+
+		expect { food.destroy }.not_to change(Food, :count)
+	end
 end
