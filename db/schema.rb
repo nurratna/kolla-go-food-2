3d2b897f9da069fbd5d6eb7647712ba1e5e269d8
nurratna <nurratnasarii@gmail.com>
2017-10-26 15:45:15 +0700

    adding scaffold and migrate cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024071844) do
+ActiveRecord::Schema.define(version: 20171026084034) do
 
   create_table "buyers", force: :cascade do |t|
     t.string "email"
     t.string "phone"
     t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
