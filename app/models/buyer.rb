4c6ac923e11e030866cd64d874ca2d29bf94eb55
nurratna <nurratnasarii@gmail.com>
2017-10-26 13:28:00 +0700

    re-commit

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,14 @@
 class Buyer < ApplicationRecord
+	validates :email, :phone, :address, presence: true
+	validates :email, uniqueness: true, format: {
+		with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+		message: "Email format not valid"
+	}
+	validates :phone, numericality: true
+	, length: { in: 10..12 }
 end
+
+# def is_a_valid_email?(email)
+#   (email =~ /^(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@{1}((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,4}$/i)
+#   email =~ /
+# end
---
+++ spec/factories/food.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+	factory :food do
+		name { Faker::Food.dish }
+		description { Faker::Food.ingredient }
+		price 10000.0
+		image_url 'image.png'
+	end
+
+	factory :invalid_food, parent: :food do
+		name nil
+		description nil
+		price 10000.0
+	end
+end
